6c88e726cd50611b4f12ce787d1fa33717e9c0e0
João Pedro Novais <129466224+jopnovais@users.noreply.github.com>
2024-08-26 20:03:57 -0300

    Update hanoi.js

--- src/hanoi.js
+++ src/hanoi.js
@@ -1,32 +1,22 @@
-// function to solve the hanoi tower and get the sequences of moves
-
 const getHanoiSolutions = (nDiscs) => {
-    const solutions = []
+  const solutions = [];
 
-    // recursive function to move the tower of discs from origin to destiny using aux as an auxiliary peg
-    const hanoi = (n, origin, destiny, aux) => {
-        if (n == 1) {
-            // Base case: If there's only one disc, move it directly to destiny
-            solutions.push({ disc: n, origin, destiny })
-            return;
-        }
-
-        // move n - 1 discs from origin to aux, using destiny as an auxiliary peg
-        hanoi(n - 1, origin, aux, destiny)
-
-        // Move the nth disc from origin to destiny
-        solutions.push({ disc: n, origin, destiny })
-
-        // Move n - 1 discs from aux to destiny, using origin as an auxiliary peg
-        hanoi(n - 1, aux, destiny, origin)
+  const hanoi = (n, origin, destiny, aux) => {
+    if (n == 1) {
+      solutions.push({ disc: n, origin, destiny });
+      return;
     }
 
-    // start the recursive process with the initial call to the hanoi function
-    hanoi(nDiscs, 0, 1, 2)
+    hanoi(n - 1, origin, aux, destiny);
 
-    return solutions;
-}
+    solutions.push({ disc: n, origin, destiny });
 
-export {
-    getHanoiSolutions
-}
+    hanoi(n - 1, aux, destiny, origin);
+  };
+
+  hanoi(nDiscs, 0, 1, 2);
+
+  return solutions;
+};
+
+export { getHanoiSolutions };
